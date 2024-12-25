bb298e557fea2809bd5cd2c2dee28c4533fe45a2
Curry-jay <860211327@qq.com>
2024-12-25 21:39:16 +0800

    初始化后端

---
+++ src/main/java/com/example/webback/WebBackApplication.java
@@ -0,0 +1,33 @@
+package com.example.webback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication
+public class WebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackApplication.class, args);
+    }
+
+
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize(DataSize.parse("300MB")); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.parse("300MB"));
+        return factory.createMultipartConfig();
+    }
+
+}
